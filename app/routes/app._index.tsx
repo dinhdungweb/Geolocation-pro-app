79cd145d7505df9699b7eab81f8b87587cae8c91
Dinhdungweb <dungmaster7@gmail.com>
2026-04-15 23:51:58 +0700

    style: increase left column height from 560px to 600px

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -272,7 +272,7 @@ export default function Index() {
             }
             .equal-height-container > .left-column {
               flex: 1;
-              height: 560px;
+              height: 600px;
               overflow: hidden;
             }
             .equal-height-container > .left-column > *,
