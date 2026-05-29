5fafd4362a703b8301e5204b5a9b0db89439a09d
Dinhdungweb <dungmaster7@gmail.com>
2026-05-29 19:25:28 +0700

    Make visitor log search indexes optional

--- prisma/migrations/20260529171000_add_visitor_log_search_indexes/migration.sql
+++ prisma/migrations/20260529171000_add_visitor_log_search_indexes/migration.sql
@@ -1,16 +1,25 @@
-CREATE EXTENSION IF NOT EXISTS pg_trgm;
+DO $$
+BEGIN
+  IF EXISTS (
+    SELECT 1
+    FROM pg_available_extensions
+    WHERE name = 'pg_trgm'
+  ) THEN
+    EXECUTE 'CREATE EXTENSION IF NOT EXISTS pg_trgm';
 
-CREATE INDEX IF NOT EXISTS "VisitorLog_ipAddress_trgm_idx"
-ON "VisitorLog" USING GIN ("ipAddress" gin_trgm_ops);
+    EXECUTE 'CREATE INDEX IF NOT EXISTS "VisitorLog_ipAddress_trgm_idx"
+      ON "VisitorLog" USING GIN ("ipAddress" gin_trgm_ops)';
 
-CREATE INDEX IF NOT EXISTS "VisitorLog_regionName_trgm_idx"
-ON "VisitorLog" USING GIN ("regionName" gin_trgm_ops);
+    EXECUTE 'CREATE INDEX IF NOT EXISTS "VisitorLog_regionName_trgm_idx"
+      ON "VisitorLog" USING GIN ("regionName" gin_trgm_ops)';
 
-CREATE INDEX IF NOT EXISTS "VisitorLog_ruleName_trgm_idx"
-ON "VisitorLog" USING GIN ("ruleName" gin_trgm_ops);
+    EXECUTE 'CREATE INDEX IF NOT EXISTS "VisitorLog_ruleName_trgm_idx"
+      ON "VisitorLog" USING GIN ("ruleName" gin_trgm_ops)';
 
-CREATE INDEX IF NOT EXISTS "VisitorLog_path_trgm_idx"
-ON "VisitorLog" USING GIN ("path" gin_trgm_ops);
+    EXECUTE 'CREATE INDEX IF NOT EXISTS "VisitorLog_path_trgm_idx"
+      ON "VisitorLog" USING GIN ("path" gin_trgm_ops)';
 
-CREATE INDEX IF NOT EXISTS "VisitorLog_userAgent_trgm_idx"
-ON "VisitorLog" USING GIN ("userAgent" gin_trgm_ops);
+    EXECUTE 'CREATE INDEX IF NOT EXISTS "VisitorLog_userAgent_trgm_idx"
+      ON "VisitorLog" USING GIN ("userAgent" gin_trgm_ops)';
+  END IF;
+END $$;
